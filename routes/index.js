708bfd340794c28c2b168cf3e574d21521c6a3d3
gabrielley <gabriel.ley@outlook.fr>
2025-10-16 13:02:46 +0200

    Important feature

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+const cowsay = require('cowsay');
+console.log(cowsay.say({text: 'Le routeur est bien créé !'}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
